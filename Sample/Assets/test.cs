497873d088d75f64be9744ac0908a61263a78af2
kougei-abe <g1727005@st.t-kougei.ac.jp>
2018-05-07 17:45:14 +0900

    2回目のコミット

--- Sample/Assets/test.cs
+++ Sample/Assets/test.cs
@@ -2,20 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        int sum = 0;
-        for(int i = 1; i <= 10; i++)
-        {
-            sum += i;
-        }
-        Debug.Log(sum);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+public class test : MonoBehaviour
+{
+    void Start()
+    {
+        Vector2 startPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos - startPos;
+        Debug.Log(dir);
+
+        float len = dir.magnitude;
+        Debug.Log(len);
+    }
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
